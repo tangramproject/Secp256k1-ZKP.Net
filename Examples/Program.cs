198378d14234b3bf9694a6fb4d378f43612ed595
pingpongsneak <pingpong@getsneak.org>
2019-02-15 01:42:10 +0200

    Updated with to_public key test. Will add more.

--- Examples/Program.cs
+++ Examples/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using Secp256k1_ZKP.Net;
 
@@ -12,33 +14,35 @@ namespace Examples
             using (var pedersen = new Pedersen())
             {
                 var blinding = secp256k1.GetSecretKey();
-                var commit = pedersen.Commit(5, blinding);
 
-                commit = pedersen.CommitParse(commit);
+                var commit = pedersen.Commit(0, blinding);
 
                 var msg = "Message for signing";
                 var msgBytes = Encoding.UTF8.GetBytes(msg);
                 var msgHash = System.Security.Cryptography.SHA256.Create().ComputeHash(msgBytes);
+
                 var sig = secp256k1.Sign(msgHash, blinding);
+
                 var pubKey = pedersen.ToPublicKey(commit);
 
-                // pubKey = secp256k1.PubKeySerialize(pubKey, Flags.SECP256K1_EC_COMPRESSED);
-
-                // Fails.....
                 var verified1 = secp256k1.Verify(sig, msgHash, pubKey);
-
-
-                // Works....
                 var pub = secp256k1.PublicKeyCreate(blinding);
-
-                var verified2 = secp256k1.Verify(sig, msgHash, pub);
-
             }
+
         }
 
         static string ToHex(byte[] data)
         {
             return BitConverter.ToString(data).Replace("-", string.Empty);
         }
+
+        public static byte[] StringToByteArray(string hex)
+        {
+            return Enumerable.Range(0, hex.Length)
+                             .Where(x => x % 2 == 0)
+                             .Select(x => Convert.ToByte(hex.Substring(x, 2), 16))
+                             .ToArray();
+        }
+
     }
 }
