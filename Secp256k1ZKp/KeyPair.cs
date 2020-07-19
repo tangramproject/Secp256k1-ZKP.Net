9a44aa85c1187833aece87e00e1c8563d070cd19
pingpongsneak <matthew.hellyer@pm.me>
2020-07-19 15:01:39 +0100

    Added secure array for key pairs.

---
+++ Secp256k1ZKp/KeyPair.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Isopoh.Cryptography.SecureArray;
+
+namespace Secp256k1Zkp
+{
+    public class KeyPair : IDisposable
+    {
+        private readonly byte[] _publicKey;
+        private readonly SecureArray<byte> _privateKey;
+
+        public KeyPair(byte[] publicKey, byte[] privateKey)
+        {
+            if (privateKey.Length % 16 != 0)
+                throw new ArgumentOutOfRangeException("Private Key length must be a multiple of 16 bytes.");
+
+            _publicKey = publicKey;
+            _privateKey = new SecureArray<byte>(32, SecureArrayType.ZeroedPinnedAndNoSwap);
+
+            Array.Copy(privateKey, _privateKey.Buffer, privateKey.Length);
+            Array.Clear(privateKey, 0, 32);
+        }
+
+        ~KeyPair()
+        {
+            Dispose();
+        }
+
+        public byte[] PublicKey => _publicKey;
+
+        public byte[] PrivateKey => _privateKey.Buffer;
+
+        public void Dispose() => _privateKey?.Dispose();
+    }
+}
